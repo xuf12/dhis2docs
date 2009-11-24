d3d72c6fca29ff2a4dd8036c3da50603eb84f44a
Jason Pickering <jason@jason-laptop>
2009-11-24 19:36:04 +0200

    Small fixes to GIS document. Made clean optional to the document builds. Execute ./build.sh with option clean.

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash 
 echo "Building HTML files"
+myargs=$1
+if [ "$1" = "clean" ] 
+then
 mvn clean docbkx:generate-html
 echo "Building PDF files"
 mvn clean docbkx:generate-pdf
+else
+mvn docbkx:generate-html
+echo "Building PDF files"
+mvn docbkx:generate-pdf
+fi
